61a456455d8f2098c3a4ede09f9a2b1b95f4f020
b1skit <mr_spikey@hotmail.com>
2018-06-19 03:07:42 -0700

    Added a loading screen to the main menu

--- Assets/UI/Scripts/MainMenuManager.cs
+++ Assets/UI/Scripts/MainMenuManager.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
+    
+    public Image loadingScreen;
 
     public void LoadLevel(int level)
     {
+        loadingScreen.transform.gameObject.SetActive(true);
+
         UnityEngine.SceneManagement.SceneManager.LoadScene(level);
 
     }
