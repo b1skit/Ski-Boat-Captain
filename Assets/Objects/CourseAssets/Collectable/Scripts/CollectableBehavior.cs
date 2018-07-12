33eadfc0f242f8b8b0d7b36c344cc8bf78b19e54
b1skit <mr_spikey@hotmail.com>
2018-07-12 03:53:05 -0700

    Implemented a basic pickup sound for the coin collectable. Created an audio mixer etc.

--- Assets/Objects/CourseAssets/Collectable/Scripts/CollectableBehavior.cs
+++ Assets/Objects/CourseAssets/Collectable/Scripts/CollectableBehavior.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CollectableBehavior : MonoBehaviour {
 
+    [Tooltip("The number of points to award for collecting this object")]
     public int pointValue = 100;
+
+    [Tooltip("The euler angles to rotate this object each frame")]
     public Vector3 rotation = new Vector3(100.0f, 0.0f, 0.0f);
 
-	// Update is called once per frame
-	void Update () {
+    [Tooltip("The sound clip to play when this objeect is collected")]
+    private AudioSource pickupSound;
+
+    private void Start()
+    {
+        pickupSound = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
         this.transform.Rotate(rotation * Time.deltaTime);
 	}
 
@@ -16,8 +27,12 @@ public class CollectableBehavior : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player" || other.gameObject.tag == "Skier")
         {
+            pickupSound.Play();
             GameManager.Instance.AddPoints(pointValue);
-            Destroy(this.gameObject);
+
+            // TEMP HACK: Sound playback is cancelled when an object is destroyed. So I destroy the mesh, then destroy the object once the sound is finished. Is there a simpler way to handle this?
+            Destroy(this.gameObject.GetComponent<MeshRenderer>());
+            Destroy(this.gameObject, pickupSound.clip.length); 
         }
     }
 }
