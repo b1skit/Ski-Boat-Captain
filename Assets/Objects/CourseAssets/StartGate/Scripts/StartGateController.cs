be24735b7917e4795431458252a3b31659661485
b1skit <mr_spikey@hotmail.com>
2018-08-06 02:36:41 +0800

    Fixed a bug where the player could recieve credit for completing a lap when they hadn't

--- Assets/Objects/CourseAssets/StartGate/Scripts/StartGateController.cs
+++ Assets/Objects/CourseAssets/StartGate/Scripts/StartGateController.cs
@@ -15,17 +15,20 @@ public class StartGateController : MonoBehaviour {
 	}
 
     private void OnTriggerEnter(Collider other) {
-        
-        // BUG HERE: Need to check we're heading the right direction so the player can't just spin in circles earning "laps"
 
-        if (other.gameObject.CompareTag("Player") && lapsRemaining == 0)
+        if (other.gameObject.CompareTag("Player") )
         {
-            SceneManager.Instance.EndLevel();
+            if (SceneManager.Instance.IsPlaying && Vector3.Dot(other.gameObject.transform.right, this.gameObject.transform.right) < 0)
+                isLegalLap = false;
+
+            if (isLegalLap && lapsRemaining == 0)
+            {
+                SceneManager.Instance.EndLevel();
+            }
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
+    private void OnTriggerExit(Collider other) {
         if (other.CompareTag("Player"))
         {
             if (SceneManager.Instance.IsPlaying && isLegalLap && Vector3.Dot(other.gameObject.transform.right, this.gameObject.transform.right) > 0)
