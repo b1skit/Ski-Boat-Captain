4889242207abafe0cf845d5d93dc83cf9ebda0c8
b1skit <mr_spikey@hotmail.com>
2018-08-13 17:59:10 +1000

    Fixed a bug where clicking a child object of a dynamic barrier would immediately unselect, as the child was destroyed during Update()

--- Assets/Objects/CourseAssets/DynamicBarrier/Scripts/DynamicBarrierController.cs
+++ Assets/Objects/CourseAssets/DynamicBarrier/Scripts/DynamicBarrierController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[ExecuteInEditMode]
+[ExecuteInEditMode, SelectionBase]
 public class DynamicBarrierController : MonoBehaviour {
 
     [Header("Dynamic barrier meshes:")]
